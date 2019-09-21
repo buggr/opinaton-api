3733c2c5e123dc8afc30cc7f45fbf95b97af3cf6
coutlcdo <coutlcdo@outlook.com>
2019-09-21 18:48:06 -0300

    fix postinstall

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.get('/coverage', (req, res) => {
     res.sendFile(__dirname + '/coverage/index.html')
 })
 
-const listener = server.listen(process.env.PORT || 3030, () => {
+const listener = server.listen(3030, () => {
     console.log("Node is listening on port: " + listener.address().port)
 })
 
