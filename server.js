8fd189fd65504199ec1a831ebb64c233ec515b7b
coutlcdo <coutlcdo@outlook.com>
2019-09-22 02:05:00 -0300

    Fix socket connection

--- server.js
+++ server.js
@@ -22,12 +22,6 @@ const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 
 app.use(cors())
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    next();
-  });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
